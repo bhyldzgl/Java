1cf607ed7019538b075e61d8311a0a487201195b
Baha Furkan Yıldızoğlu <129391111+bhyldzgl@users.noreply.github.com>
2024-02-05 21:11:35 +0300

    Add files via upload

---
+++ Java1/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int Number1 = 10;
+        
+        int Number2 = Number1 * 2;
+        
+        int Number3 = Number1 * Number2 * 2;
+        
+        int Total = Number1 + Number2 + Number3;
+        
+        Total = Total - 5;
+        System.out.println("Total = " + Total);
+                
+    }
+    
+}
