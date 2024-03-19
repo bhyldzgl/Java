c32118b474367295809cc51fbbcd56980fddfcae
Baha Furkan Yıldızoğlu <129391111+bhyldzgl@users.noreply.github.com>
2024-03-19 17:03:52 +0300

    Add files via upload

---
+++ 4-String/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        
+       String a ="Java\tProgramming\tLanguage";
+       
+        System.out.println(a);
+       
+    }
+    
+}
