beae32cec4fee03f0c18f74238305ace8128a37d
Baha Furkan Yıldızoğlu <129391111+bhyldzgl@users.noreply.github.com>
2024-03-19 17:10:52 +0300

    Add files via upload

---
+++ educational-projects/2-fuel-amount-calculation/src/Main.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("How many cents does your car burn per kilometer? :");
+        double money = scanner.nextDouble();
+        
+        System.out.print("How many kilometers did you travel with your vehicle? :");
+        
+        int km = scanner.nextInt();
+        
+        System.out.println("Total amount you need to pay : " + (money * km));
+        
+    }
+    
+}
