7df0fa45f322a10699f9e9294bfbece3303c38ab
Baha Furkan Yıldızoğlu <129391111+bhyldzgl@users.noreply.github.com>
2024-08-12 17:52:23 +0300

    Add files via upload

---
+++ 2 11 comparasion operators/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+ 
+    public static void main(String[] args) {
+         
+           /*
+             ==  --> is it equal?
+             !=  --> isn't equal? 
+             >   --> is it big ?
+             >=  --> greater than or equal to
+             <   --> is it small ?
+             <=  --> less than or equal to 
+               
+           */
+           
+           System.out.println(3 == 3);
+           System.out.println(3 == 4);
+           System.out.println(3 != 3);
+           System.out.println(2 > 1);
+           System.out.println(2 < 1);
+           
+    }
+    
+}
+ 
